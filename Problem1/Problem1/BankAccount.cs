57ff14154f897a5814d8fc5e6b4b1a84a3437409
nhan <nhan4599>
2019-10-17 12:58:21 +0700

    ok \

---
+++ Problem1/Problem1/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class BankAccount
+    {
+        int money;
+        public BankAccount(int money)
+        {
+            this.money = money;
+        }
+
+        public void Withdraw(int amount)
+        {
+            if ( amount < money)
+            {
+                money -= amount;
+            }
+        }
+        public void Deposit(int amount)
+        {
+            money += amount;
+        }
+        public int Balance()
+        {
+
+            return money;
+        }
+
+    }
+}
---
+++ Problem1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem1;
+
+namespace UnitTestProblem1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodBankAccount()
+        {
+            BankAccount acc  = new BankAccount(200000);
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(BankAccount));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+        [TestMethod]
+        public void testWith()
+        {
+            BankAccount acc = new BankAccount(200000);
+            acc.Withdraw(50000);
+            Assert.AreEqual(150000, acc.Balance());
+            acc.Withdraw(20000);
+            Assert.AreEqual(130000, acc.Balance());
+        }
+        [TestMethod]
+        public void Deposit()
+        {
+            BankAccount acc = new BankAccount(200000);
+            acc.Deposit(50000);
+            Assert.AreEqual(250000, acc.Balance());
+            acc.Deposit(10000);
+            Assert.AreEqual(260000, acc.Balance());
+        }
+       
+
+
+    }
+}
